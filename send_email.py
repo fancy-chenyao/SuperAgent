a55ee02e346dfa706b21b4b926d85c924555ee55
fanchenyao <632326211@qq.com>
2026-03-09 11:01:10 +0800

    chore: 清理无用文件和更新.gitignore
    删除不再使用的报告文件和邮件发送脚本
    更新.gitignore以更好地管理IDE、构建和日志文件

--- send_email.py
+++
@@ -1,7 +0,0 @@
-import smtplib 
-import ssl 
-from email.mime.text import MIMEText 
-from email.mime.multipart import MIMEMultipart 
-from email.mime.application import MIMEApplication 
-from datetime import datetime 
-import os 
